6ecec59dec8eeec0ae23bb85ec652c0c8cc19331
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-05-27 16:25:05 -0700

    fix: move to Descartes

--- src/rspn/uhf_ccsd/test_uhf_ccsd_lr.py
+++ src/rspn/uhf_ccsd/test_uhf_ccsd_lr.py
@@ -1,21 +1,22 @@
 import pickle
 
 from chem.ccsd.uhf_ccsd import UHF_CCSD
-from rspn.uhf_ccsd.uhf_ccsd_lr import UHF_CCSD_LR, CART
+from chem.meta.coordinates import CARTESIAN
+from rspn.uhf_ccsd.uhf_ccsd_lr import UHF_CCSD_LR
 import numpy as np
 from scipy.sparse.linalg import gmres
 
 
-def basic_test():
+def t_response_test():
     with open('uhf_ccsd.pkl','rb') as bak_file:
         ccsd: UHF_CCSD = pickle.load(bak_file)
     lr = UHF_CCSD_LR(ccsd.data, ccsd.scf_data)
     cc_jacobian = lr.build_the_cc_jacobian()
     cc_mu = {}
-    for coord in CART:
-        cc_mu[coord] = lr.build_the_cc_dipole(coord)
+    for coord in CARTESIAN:
+        cc_mu[coord] = lr._build_the_cc_dipole_helper(coord)
 
-    for coord in CART:
+    for coord in CARTESIAN:
         sol, exit_code = gmres(cc_jacobian, cc_mu[coord], rtol=1e-7, atol=1e-7)
         if exit_code != 0:
             raise RuntimeError('GMRES didn\'t find the response vector!')
@@ -24,4 +25,4 @@ def basic_test():
 
 
 if __name__ == "__main__":
-    basic_test()
+    t_response_test()
--- src/rspn/uhf_ccsd/uhf_ccsd_lr.py
+++ src/rspn/uhf_ccsd/uhf_ccsd_lr.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
 from chem.ccsd.uhf_ccsd import UHF_CCSD_Data
 from chem.hf.intermediates_builders import Intermediates
+from chem.meta.coordinates import Descartes, CARTESIAN
 import numpy as np
+from numpy.typing import NDArray
+from scipy.sparse.linalg import gmres
 from rspn.uhf_ccsd.equations.dipole.singles import (
     get_mux_aa,
     get_mux_bb,
@@ -17,15 +20,35 @@ from rspn.uhf_ccsd.equations.singles_singles import (
     get_singles_singles_bbbb,
 )
 
-CART = ['x', 'y', 'z']
-
 @dataclass
 class UHF_CCSD_LR:
     uhf_ccsd_data: UHF_CCSD_Data
     uhf_scf_data: Intermediates
 
-    def find_t_response(self):
+    def find_polarizabilities(self):
         cc_jacobian = self.build_the_cc_jacobian()
+        cc_electric_dipole = self.build_cc_electric_dipole_singles()
+        t_response = self.find_t_response(cc_jacobian, cc_electric_dipole)
+    
+    def find_t_response(
+        self,
+        cc_jacobian: NDArray,
+        cc_mu: dict[Descartes, NDArray],
+    ) -> dict[Descartes, NDArray]:
+
+        t_response_mu = {}
+        for coord in CARTESIAN:
+            response, exit_code = gmres(
+                cc_jacobian,
+                cc_mu[coord],
+                rtol=1e-7,
+                atol=1e-7,
+            )
+            if exit_code != 0:
+                msg = f'gmres didn\'t find the response vector for mu {coord}'
+                raise RuntimeError(msg)
+            t_response_mu[coord] = response
+        return t_response_mu
 
     def assign_dims(self):
         """ TODO: make it work alright """
@@ -64,28 +87,45 @@ class UHF_CCSD_LR:
         jacobian = np.block([[aaaa, aabb], [bbaa, bbbb]])
         return jacobian
 
+    def build_cc_electric_dipole_singles(self) -> dict[Descartes, NDArray]:
+        r"""
+        Builds the matrices:
 
-    def build_the_cc_dipole(self, coord: str):
-        if coord not in CART:
-            raise ValueError(f'coordinates got to be one of {CART}.')
+        matrix[x][a, i] = <HF| e^{-T} a*(i) a(a) \hat{mu} _x E^{T} |HF>
 
+        the flipped order of `a` and `i` appears because the pair (a, i) is an
+        index of a single substitution $\tau _mu ^\dagger$. The theory from
+        Ref. [1] works on the substitution indices.
+
+        [1] H. Koch and P. Jørgensen, Coupled cluster response functions, The
+        Journal of Chemical Physics 93, 3333 (1990).
+        """
+        cc_mu = {}
+        for coord in CARTESIAN:
+            cc_mu[coord] = self._build_the_cc_dipole_helper(coord)
+        return cc_mu
+
+    def _build_the_cc_dipole_helper(self, coord: Descartes):
         scf = self.uhf_scf_data
         ccsd = self.uhf_ccsd_data
         self.assign_dims()
         dim_aa = self.dims['aa']
         dim_bb = self.dims['bb']
 
-        if coord == 'x':
+        if coord == Descartes.x:
             aa = get_mux_aa(scf, ccsd).reshape(dim_aa)
             bb = get_mux_bb(scf, ccsd).reshape(dim_bb)
             return np.block([aa, bb])
 
-        elif coord == 'y':
+        elif coord == Descartes.y:
             aa = get_muy_aa(scf, ccsd).reshape(dim_aa)
             bb = get_muy_bb(scf, ccsd).reshape(dim_bb)
             return np.block([aa, bb])
 
-        elif coord == 'z':
+        elif coord == Descartes.z:
             aa = get_muz_aa(scf, ccsd).reshape(dim_aa)
             bb = get_muz_bb(scf, ccsd).reshape(dim_bb)
             return np.block([aa, bb])
+
+        else:
+            raise ValueError(f"Unknown cartesian coordinate: {coord}.")
