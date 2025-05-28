e62323bc216da6c074a80d0c68ba2f61a9a05c51
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-05-27 17:36:27 -0700

    Add the CC Jacobian diagonalization test

---
+++ src/rspn/uhf_ccsd/test_cc_jacobian_diagonalization.py
@@ -0,0 +1,23 @@
+import pickle
+from chem.ccsd.uhf_ccsd import UHF_CCSD
+from rspn.uhf_ccsd.uhf_ccsd_lr import UHF_CCSD_LR
+import numpy as np
+
+
+def test():
+    with open('uhf_ccsd.pkl','rb') as bak_file:
+       ccsd: UHF_CCSD = pickle.load(bak_file)
+
+    lr = UHF_CCSD_LR(ccsd.data, ccsd.scf_data)
+    cc_jacobian = lr.build_the_cc_jacobian()
+    evals, evecs = np.linalg.eig(cc_jacobian)
+    # The cc jacobian matrix is non-symmetric so a transpose, in general, does
+    # not produce the inverse
+    assert not np.allclose(evecs.T @ cc_jacobian @ evecs, np.diag(evals))
+    # The inverse needs to be calculated
+    inv_evecs = np.linalg.inv(evecs)
+    assert np.allclose(inv_evecs @ cc_jacobian @ evecs, np.diag(evals))
+
+
+if __name__ == "__main__":
+    test()
