8d0849264ee0691769d81da037f9944fe7f28283
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-06-18 13:36:00 -0400

    test: water UHF-CCS/STO-3G

--- tests/uhf_ccsd/water-sto3g/prepare_water_sto3g.py
+++ tests/uhf_ccsd/water-sto3g/prepare_water_sto3g.py
@@ -1,42 +1,26 @@
 import pickle
 
-import psi4
-from psi4.core import Molecule, Wavefunction
+from chem.hf.electronic_structure import hf
 from chem.hf.intermediates_builders import extract_intermediates
 from chem.ccsd.uhf_ccsd import UHF_CCSD
 
 
-def scf() -> tuple[Molecule, float, Wavefunction]:
+def solve_and_save():
     """ Geometry from CCCBDB: HF/STO-3G """
-    mol: Molecule = psi4.geometry("""
+    geometry = """
     0 1
     O1	0.0000   0.0000   0.1272
     H2	0.0000   0.7581  -0.5086
     H3	0.0000  -0.7581  -0.5086
     symmetry c1
-    """)
-
-    psi4.set_options({'basis': 'sto-3g',
-                      'scf_type': 'pk',
-                      'e_convergence': 1e-12,
-                      'd_convergence': 1e-12})
-
-    psi4.core.be_quiet()
-
-    # compute the Hartree-Fock energy and wavefunction
-    energy, wfn = psi4.energy('SCF', molecule=mol, return_wfn=True)
-
-    return mol, energy, wfn
-
-
-def solve_and_save():
-    _, _, wfn = scf()
+    """
+    _, _, wfn = hf(geometry=geometry, basis='sto-3g')
     intermediates = extract_intermediates(wfn)
     ccsd = UHF_CCSD(intermediates)
     ccsd.verbose = 1
     ccsd.solve_cc_equations()
     ccsd.solve_lambda_equations()
-    with open('pickles/water_sto3g.pkl','wb') as bak_file:
+    with open('pickles/water_sto3g.pkl', 'wb') as bak_file:
         pickle.dump(ccsd, bak_file)
 
 
