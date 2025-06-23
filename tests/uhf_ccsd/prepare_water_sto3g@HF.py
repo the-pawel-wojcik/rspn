df4978f7a56c9b923a5ec5ac459fe17261223dcc
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-06-23 16:08:19 -0400

    chore: Use unified prepare for water STO3G

--- tests/uhf_ccsd/prepare_water_sto3g@HF.py
+++ tests/uhf_ccsd/prepare_water_sto3g@HF.py
@@ -5,7 +5,7 @@ from chem.hf.intermediates_builders import extract_intermediates
 from chem.ccsd.uhf_ccsd import UHF_CCSD
 
 
-def solve_and_save():
+def solve_and_save_water_sto3g_at_HF():
     """ Geometry from CCCBDB: HF/STO-3G """
     geometry = """
     0 1
@@ -14,15 +14,15 @@ def solve_and_save():
     H3	0.0000  -0.7581  -0.5086
     symmetry c1
     """
-    _, _, wfn = hf(geometry=geometry, basis='sto-3g')
-    intermediates = extract_intermediates(wfn)
+    hf_result = hf(geometry=geometry, basis='sto-3g')
+    intermediates = extract_intermediates(hf_result.wfn)
     ccsd = UHF_CCSD(intermediates)
     ccsd.verbose = 1
     ccsd.solve_cc_equations()
     ccsd.solve_lambda_equations()
-    with open('pickles/water_sto3g.pkl', 'wb') as bak_file:
+    with open('pickles/water_sto3g@HF.pkl', 'wb') as bak_file:
         pickle.dump(ccsd, bak_file)
 
 
 if __name__ == "__main__":
-    solve_and_save()
+    solve_and_save_water_sto3g_at_HF()
