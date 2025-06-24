798c2bf2730d574398df6019d7b5bc3d31f025af
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-06-24 11:58:46 -0400

    test: sigma build, brute force vs elegant

--- tests/uhf_ccsd/test_cc_jacobian_action.py
+++ tests/uhf_ccsd/test_cc_jacobian_action.py
@@ -10,44 +10,60 @@ from rspn.uhf_ccsd.equations.cc_jacobian_contract_Rain.singles import (
     get_cc_j_w_singles_aa,
     get_cc_j_w_singles_bb,
 )
+from rspn.uhf_ccsd.equations.cc_jacobian_contract_Rain.doubles import (
+    get_cc_j_w_doubles_aaaa,
+    get_cc_j_w_doubles_abab,
+    get_cc_j_w_doubles_abba,
+    get_cc_j_w_doubles_baab,
+    get_cc_j_w_doubles_baba,
+    get_cc_j_w_doubles_bbbb,
+)
 import numpy as np
 
 
-BLOCKS = [ 'aa', 'bb', 'aaaa', 'abab', 'abba', 'baab', 'baba', 'bbbb', ]
+def build_elegant_sigma(test_vector: NDArray, ccsd: UHF_CCSD, **kwargs):
+    mock_rhs = Spin_MBE.from_flattened_NDArray(test_vector, ccsd.scf_data)
+    sigma_elegant = Spin_MBE()
+    sigma_elegant.singles[E1_spin.aa] = get_cc_j_w_singles_aa(
+        **kwargs,
+        vector=mock_rhs,
+    )
+    sigma_elegant.singles[E1_spin.bb] = get_cc_j_w_singles_bb(
+        **kwargs,
+        vector=mock_rhs,
+    )
 
+    sigma_elegant.doubles[E2_spin.aaaa] = get_cc_j_w_doubles_aaaa(
+        **kwargs,
+        vector=mock_rhs,
+    )
+    sigma_elegant.doubles[E2_spin.aaaa] = get_cc_j_w_doubles_aaaa(
+        **kwargs,
+        vector=mock_rhs,
+    )
+    sigma_elegant.doubles[E2_spin.abab] = get_cc_j_w_doubles_abab(
+        **kwargs,
+        vector=mock_rhs,
+    )
+    sigma_elegant.doubles[E2_spin.abba] = get_cc_j_w_doubles_abba(
+        **kwargs,
+        vector=mock_rhs,
+    )
+    sigma_elegant.doubles[E2_spin.baab] = get_cc_j_w_doubles_baab(
+        **kwargs,
+        vector=mock_rhs,
+    )
+    sigma_elegant.doubles[E2_spin.baba] = get_cc_j_w_doubles_baba(
+        **kwargs,
+        vector=mock_rhs,
+    )
+    sigma_elegant.doubles[E2_spin.bbbb] = get_cc_j_w_doubles_bbbb(
+        **kwargs,
+        vector=mock_rhs,
+    )
 
-def get_singles_dim(dims: dict[str, int]) -> int:
-    return sum(dims[block] for block in E1_spin)
+    return sigma_elegant
 
-
-def get_doubles_dim(dims: dict[str, int]) -> int:
-    return sum(dims[block] for block in E2_spin)
-
-
-def get_vector_dim(dims: dict[str, int]) -> int:
-    singles_dim = get_singles_dim(dims)
-    doubles_dim = get_doubles_dim(dims)
-    return singles_dim + doubles_dim
-
-
-def vector_to_mbe(vector: NDArray, dims: dict[str, int]) -> Spin_MBE:
-
-    assert len(vector.shape) == 1
-    assert vector.shape[0] == get_vector_dim(dims)
-
-    mbe = Spin_MBE()
-    dim_sum = 0
-    for block in E1_spin:
-        block_dim = dims[block]
-        mbe.singles[block] = vector[dim_sum: dim_sum + block_dim]
-        dim_sum += block_dim
-
-    for block in E2_spin:
-        block_dim = dims[block]
-        mbe.doubles[block] = vector[dim_sum: dim_sum + block_dim]
-        dim_sum += block_dim
-
-    return mbe
 
 
 def test_CC_Jacobian_action():
@@ -62,7 +78,7 @@ def test_CC_Jacobian_action():
     dims = lr.assign_dims()
     cc_jacobian = build_cc_jacobian(
         kwargs=kwargs,
-        dims=lr.assign_dims(),
+        dims=dims,
     )
 
     # Generate ten test vectors
@@ -73,18 +89,17 @@ def test_CC_Jacobian_action():
 
     for test_vector in test_vectors:
         out_vector = cc_jacobian @ test_vector
-        sigma_brute_force = vector_to_mbe(out_vector, dims)
+        sigma_brute_force = Spin_MBE.from_flattened_NDArray(
+            vector=out_vector, uhf_scf_data=ccsd.scf_data,
+        )
         sigma_brute_force.pretty_print_mbe()
-        mock_rhs = vector_to_mbe(test_vector, dims)
-        sigma_elegant = Spin_MBE()
-        sigma_elegant.singles[E1_spin.aa] = get_cc_j_w_singles_aa(
-            **kwargs,
-            vector=mock_rhs,
-        )
-        sigma_elegant.singles[E1_spin.bb] = get_cc_j_w_singles_bb(
-            **kwargs,
-            vector=mock_rhs,
-        )
+
+        sigma_elegant = build_elegant_sigma(test_vector, ccsd, **kwargs)
+        sigma_elegant.pretty_print_mbe()
+
+        # for spin_block, elegant in sigma_elegant.singles.items():
+        #     assert np.allclose(elegant, sigma_brute_force.singles[spin_block])
+
         break
 
 
