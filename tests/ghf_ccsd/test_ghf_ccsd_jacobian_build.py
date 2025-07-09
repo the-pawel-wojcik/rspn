9fb23e69735e19824f49c1d02466e5c8831b3121
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-07-09 15:57:20 -0400

    test: CC Jacobian elements doubles

--- tests/ghf_ccsd/test_ghf_ccsd_jacobian_build.py
+++ tests/ghf_ccsd/test_ghf_ccsd_jacobian_build.py
@@ -1,12 +1,13 @@
 from contextlib import contextmanager
+from itertools import product
 import pickle
-from numpy.typing import NDArray
 import pytest
 from time import perf_counter
 
 from chem.ccsd.ghf_ccsd import GHF_CCSD
 from chem.ccsd.equations.ghf.util import GHF_Generators_Input
 import numpy as np
+from numpy.typing import NDArray
 from rspn.ghf_ccsd.ghf_ccsd_lr import build_cc_jacobian
 from rspn.ghf_ccsd.equations.cc_jacobian.singles_singles import (
     get_cc_j_singles_singles,
@@ -22,6 +23,9 @@ from rspn.ghf_ccsd.equations.cc_jacobian.doubles_doubles import (
 )
 
 
+ALL_CLOSE_THRESH = 1e-9
+
+
 def humanify(size_bytes: float) -> str:
     for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti']:
         if abs(size_bytes) < 1024.0:
@@ -31,7 +35,6 @@ def humanify(size_bytes: float) -> str:
 
 
 # this one takes too long for a regular test
-@pytest.mark.skip
 def test_cc_jacobian_build():
     with open('pickles/water_sto3g@HF.pkl', 'rb') as bak_file:
         ccsd: GHF_CCSD = pickle.load(bak_file)
@@ -73,7 +76,6 @@ def test_cc_jacobian_build():
     print()
 
 
-@pytest.mark.skip
 def test_cc_jacobian_spectrum():
     with open('pickles/water_sto3g@HF.pkl', 'rb') as bak_file:
         ccsd: GHF_CCSD = pickle.load(bak_file)
@@ -101,47 +103,124 @@ def timeit(header: str=''):
     print(f'[{end-start:.3f} s] {header} time.')
 
 
-def compare_singles(jacobian: NDArray, singles_singles: NDArray) -> None:
-    print(f'{singles_singles.shape=}')
-    print('0,0 - singles')
-    print(f'  {jacobian[0,0:10]}\n  {singles_singles[0,0,0,:]}')
-    print(f'  {jacobian[0,10:20]}\n  {singles_singles[0,0,1,:]}')
-    print(f'  {jacobian[0,20:30]}\n  {singles_singles[0,0,2,:]}')
-    print(f'  {jacobian[0,30:40]}\n  {singles_singles[0,0,3,:]}')
-    print('0,1 - singles')
-    print(f'  {jacobian[1,0:10]}\n  {singles_singles[0,1,0,:]}')
-    print(f'  {jacobian[1,10:20]}\n  {singles_singles[0,1,1,:]}')
-    print(f'  {jacobian[1,20:30]}\n  {singles_singles[0,1,2,:]}')
-    print(f'  {jacobian[1,30:40]}\n  {singles_singles[0,1,3,:]}')
-    print('3,9 - singles')
-    print(f'  {jacobian[39,0:10]}\n  {singles_singles[3,9,0,:]}')
-    print(f'  {jacobian[39,10:20]}\n  {singles_singles[3,9,1,:]}')
-    print(f'  {jacobian[39,20:30]}\n  {singles_singles[3,9,2,:]}')
-    print(f'  {jacobian[39,30:40]}\n  {singles_singles[3,9,3,:]}')
+def singles_mbidx_to_idx(sv: int, so: int) -> int:
+    return sv * 10 + so
+
+
+def doubles_mbidx_to_idx(dvl: int, dvr: int, dol:int, dor: int) -> int:
+    return dor + 10 * dol + 100 * dvr + 400 * dvl + 40
+
+
+def compare_singles(
+    jacobian: NDArray,
+    singles_singles: NDArray,
+    verbose: bool = False,
+) -> None:
+    assert singles_singles.shape == (4, 10, 4, 10)
+    # lsv = left singles virtual
+    for lsv in range(4):
+        for lso in range(10):
+            for rsv in range(4):
+                if verbose is True:
+                    print(f'singles({lsv},{lso}) - singles({rsv}, :)')
+                    left = singles_mbidx_to_idx(sv=lsv, so=lso)
+                    right_start = singles_mbidx_to_idx(sv=rsv, so=0)
+                    jacobian_block = jacobian[left,right_start:right_start+10] 
+                    ss_block = singles_singles[lsv, lso, rsv, :]
+                    assert np.allclose(
+                        jacobian_block,
+                        ss_block,
+                        ALL_CLOSE_THRESH
+                    )
+                    if verbose is True:
+                        jb_norm = np.linalg.norm(jacobian_block)
+                        ssb_norm = np.linalg.norm(ss_block)
+                        assert np.isclose(jb_norm, ssb_norm, ALL_CLOSE_THRESH)
+                        print(f'  {jacobian_block}')
+                        print(f'  {ss_block}')
 
 
 def compare_sd(
     jacobian: NDArray,
-    sd: NDArray,
-    singles: tuple[int, int] = (0, 0),
-    doubles: tuple[int, int] = (0, 0),
+    singles_doubles: NDArray,
+    sv: int = 0,
+    doubles_vv: tuple[int, int] = (0, 0),
+    verbose: bool = False,
 ) -> None:
-    sv = singles[0]
-    so = singles[1]
-    lft = 10 * sv + so
-    dvl = doubles[0]  # doubles virtual left
-    dvr = doubles[1]  # doubles virtual right
+    """ Compare that the blocks
+    singles_doubles[(sv, :), (doubles_vv[0], doubles_vv[1], :, :)]
+    are the same in both the singles_doubles and the cc_jacobian matrices.
+    In other words it makes sure that the mappings between the many-body index
+    works. """
+    dvl = doubles_vv[0]  # doubles virtual left
+    dvr = doubles_vv[1]  # doubles virtual right
     for so in range(10):
-        print(f'{sv},{so} - doubles ({dvl},{dvr},:,:)')
-        base = 40 + 100 * (dvr + 4 * dvl)
+        if verbose is True:
+            print(f'{sv},{so} - doubles ({dvl},{dvr},:,:)')
+
+        lft = singles_mbidx_to_idx(sv, so)
         for dol in range(10):
-            jac_block = jacobian[lft,base + 10 * dol:base + 10 * (dol + 1)]
-            sd_block = sd[sv,so,dvl,dvr,dol,:]
+            right_start = doubles_mbidx_to_idx(dvl, dvr, dol, 0)
+            jac_block = jacobian[lft, right_start:right_start + 10]
+            sd_block = singles_doubles[sv,so,dvl,dvr,dol,:]
             jack_norm = np.linalg.norm(jac_block)
-            if jack_norm > 1e-3:
+            if verbose is True and jack_norm > 1e-3:
                 print(f'  {jac_block}')
                 print(f'  {sd_block}')
-            assert np.allclose(jac_block, sd_block, atol=1e-6)
+            assert np.allclose(jac_block, sd_block, atol=ALL_CLOSE_THRESH)
+
+
+def compare_ds(
+    jacobian: NDArray,
+    doubles_singles: NDArray,
+    doubles_vv: tuple[int, int] = (0, 0),
+    sv: int = 0,
+    verbose: bool = False,
+) -> None:
+    assert doubles_singles.shape == (4, 4, 10, 10, 4, 10)
+    dvl = doubles_vv[0]  # doubles virtual left
+    dvr = doubles_vv[1]  # doubles virtual right
+    for so in range(10):
+        if verbose is True:
+            print(f'doubles({dvl},{dvr},:,:) - singles({sv},{so})')
+
+        right = singles_mbidx_to_idx(sv, so)
+        for dol in range(10):
+            left_start = doubles_mbidx_to_idx(dvl, dvr, dol, 0)
+            jacobian_block = jacobian[left_start:left_start + 10, right]
+            ds_block = doubles_singles[dvl, dvr, dol, :, sv, so]
+            jacobian_block_norm = np.linalg.norm(jacobian_block)
+            if verbose is True and jacobian_block_norm > 1e-3:
+                print(f'  {jacobian_block}')
+                print(f'  {ds_block}')
+            assert np.allclose(jacobian_block, ds_block, atol=ALL_CLOSE_THRESH)
+
+
+def compare_doubles(
+    jacobian: NDArray,
+    doubles_doubles: NDArray,
+    verbose: bool = False,
+) -> None:
+    assert doubles_doubles.shape == (4, 4, 10, 10, 4, 4, 10, 10)
+    # lvl = left virtual left
+    # lor = left occupied right
+    for lvl, lvr, lol, lor, rvl, rvr, rol in product(
+        range(4), range(4), range(10), range(10), range(4), range(4), range(10)
+    ):
+        if verbose is True:
+            print(f'doubles({lvl}, {lvr}, {lol}, {lor}) -', end='')
+            print(f' doubles({rvl}, {rvr}, {rol}, :)')
+        left = doubles_mbidx_to_idx(dvl=lvl, dvr=lvr, dol=lol, dor=lor)
+        right_start = doubles_mbidx_to_idx(dvl=rvl, dvr=rvr, dol=rol, dor=0)
+        jacobian_block = jacobian[left,right_start:right_start+10] 
+        ss_block = doubles_doubles[lvl, lvr, lol, lor, rvl, rvr, rol, :]
+        assert np.allclose(jacobian_block, ss_block, ALL_CLOSE_THRESH)
+        if verbose is True:
+            jb_norm = np.linalg.norm(jacobian_block)
+            ssb_norm = np.linalg.norm(ss_block)
+            assert np.isclose(jb_norm, ssb_norm, ALL_CLOSE_THRESH)
+            print(f'  {jacobian_block}')
+            print(f'  {ss_block}')
 
 
 def test_cc_jacobian_to_NDArray_translation():
@@ -180,14 +259,22 @@ def test_cc_jacobian_to_NDArray_translation():
         ])
 
     np.set_printoptions(precision=3, suppress=True)
-    # compare_singles(jacobian, singles_singles)
-    compare_sd(jacobian, singles_doubles, doubles=(0, 1))
-    
+    with timeit('Checking singles-singles'):
+        compare_singles(jacobian, singles_singles)
+    for sv in range(4):
+        for dvl in range(4):
+            for dvr in range(4):
+                compare_sd(jacobian, singles_doubles,
+                           sv=sv, doubles_vv=(dvl, dvr))
 
-    print(f'{np.linalg.norm(singles_singles)=}')
-    print(f'{np.linalg.norm(singles_doubles)=}')
-    print(f'{np.linalg.norm(doubles_singles)=}')
-    print(f'{np.linalg.norm(doubles_doubles)=}')
+    for sv in range(4):
+        for dvl in range(4):
+            for dvr in range(4):
+                compare_ds(jacobian, doubles_singles,
+                           sv=sv, doubles_vv=(dvl, dvr))
+
+    with timeit('Checking doubles-doubles'):
+        compare_doubles(jacobian, doubles_doubles)
 
 
 if __name__ == "__main__":
