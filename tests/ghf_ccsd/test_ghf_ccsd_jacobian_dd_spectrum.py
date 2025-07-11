44a12ee78a58625365fe9f09af6788ce81a00a8a
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-07-11 10:16:53 -0400

    test: GHF-CCSD Jacobian DD spectrum

---
+++ tests/ghf_ccsd/test_ghf_ccsd_jacobian_dd_spectrum.py
@@ -0,0 +1,29 @@
+import pickle
+
+from chem.ccsd.ghf_ccsd import GHF_CCSD
+from chem.ccsd.equations.ghf.util import GHF_Generators_Input
+import numpy as np
+from rspn.ghf_ccsd.ghf_ccsd_lr import build_cc_jacobian
+
+
+def test_cc_jacobian_spectrum():
+    with open('pickles/water_sto3g@HF.pkl', 'rb') as bak_file:
+        ccsd: GHF_CCSD = pickle.load(bak_file)
+
+    builders_input = GHF_Generators_Input(
+        ghf_data=ccsd.ghf_data,
+        ghf_ccsd_data=ccsd.data,
+    )
+
+    no = ccsd.ghf_data.no
+    nv = ccsd.ghf_data.nv
+    dim_s = nv * no
+    cc_jacobian = build_cc_jacobian(builders_input)
+    cc_jacobian_dd = cc_jacobian[dim_s:, dim_s:]
+    dd_eigenvalues = np.linalg.eigvals(cc_jacobian_dd)
+    for eval in sorted(dd_eigenvalues, key=lambda z: abs(z)):
+        print(f'{eval.real:+12.6f}', end='')
+        if abs(eval.imag) > 1e-6:
+            print(f'{eval.real:+12.6f}i')
+        else:
+            print('')
