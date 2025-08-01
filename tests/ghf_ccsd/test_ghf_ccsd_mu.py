311712c378747e81e61f8673086c7b6177552b8e
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-08-01 16:57:10 -0400

    test: print doubles in the psi4 style

--- tests/ghf_ccsd/test_ghf_ccsd_mu.py
+++ tests/ghf_ccsd/test_ghf_ccsd_mu.py
@@ -1,13 +1,35 @@
+import itertools
 from chem.ccsd.equations.ghf.util import GHF_Generators_Input
 from chem.ccsd.ghf_ccsd import GHF_CCSD
+from chem.hf.ghf_data import GHF_Data
 from chem.meta.coordinates import Descartes
 import numpy as np
+from numpy.typing import NDArray
 from rspn.ghf_ccsd._nuOpCC import build_nu_bar_V_cc
 
 
+def print_doubles_as_psi4(doubles: NDArray, ghf_data: GHF_Data) -> None:
+    assert len(doubles.shape) == 4
+    no = ghf_data.no
+    nv = ghf_data.nv
+    assert doubles.shape == (nv, nv, no, no)
+
+    print(' ' * 6, end='')
+    for a, b in itertools.product(range(0, nv, 2), range(0, nv, 2)):
+        print(f'  ({a}, {b})', end='')
+    print()
+    fmt = ' 6.4f'
+    for i, j in itertools.product(range(0, no, 2), range(0, no, 2)):
+        print(f'({i}, {j}) ', end='')
+        for a, b in itertools.product(range(0, nv, 2), range(0, nv, 2)):
+            print(f'{doubles[a][b][i][j]:{fmt}} ', end='')
+        print()
+
+
 def test_cc_mu(ghf_ccsd_water_sto3g: GHF_CCSD) -> None:
+    ghf_data = ghf_ccsd_water_sto3g.ghf_data
     input = GHF_Generators_Input(
-        ghf_data=ghf_ccsd_water_sto3g.ghf_data,
+        ghf_data=ghf_data,
         ghf_ccsd_data=ghf_ccsd_water_sto3g.data,
     )
     cced_interation_op = build_nu_bar_V_cc(input=input)
@@ -67,5 +89,7 @@ def test_cc_mu(ghf_ccsd_water_sto3g: GHF_CCSD) -> None:
             mubar['singles'], PSI4_MU_BAR_IA[direction].T, atol=1e-8,
         )
 
-
         assert mubar['doubles'].shape == (4, 4, 10, 10)
+        print()
+        print(direction)
+        print_doubles_as_psi4(mubar['doubles'], ghf_data)
